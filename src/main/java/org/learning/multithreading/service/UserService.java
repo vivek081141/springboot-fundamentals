53e21625977c7dfc9799c8b16b51c530ac583f8c
Vivek Agrawal <vivek.agrawal01@sap.com>
2020-03-29 12:37:51 +0530

    Async

--- src/main/java/org/learning/multithreading/service/UserService.java
+++ src/main/java/org/learning/multithreading/service/UserService.java
@@ -1,7 +1,12 @@
 package org.learning.multithreading.service;
 
+import org.learning.multithreading.entity.User;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public interface UserService {
-    void saveUser(MultipartFile file);
+    CompletableFuture<List<User>> saveUser(MultipartFile file);
+    List<User> saveUsers(MultipartFile file);
 }
