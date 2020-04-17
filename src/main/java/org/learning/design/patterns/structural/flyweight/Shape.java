111f7712a3ff20c9fc719ed5d77e40bc8dc5c52f
Vivek Agrawal <vivek081141@gmail.com>
2020-04-18 00:15:22 +0530

    Feature (#8)
    * feature 1
    
    * feature 2
    
    * Design patterns - creational
    
    * Factory - Design patterns
    
    * Decorator Pattern Added
    
    * Strategy Pattern
    
    Co-authored-by: Vivek Agrawal <vivek.agrawal01@sap.com>

---
+++ src/main/java/org/learning/design/patterns/structural/flyweight/Shape.java
@@ -0,0 +1,5 @@
+package org.learning.design.patterns.structural.flyweight;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/test/java/org/learning/design/patterns/behaviour/strategy/ThreadStrategyDemoTest.java
@@ -0,0 +1,51 @@
+/*
+ * All rights reserved 2020
+ * author: vivek081141@gmail.com
+ *
+ */
+
+package org.learning.design.patterns.behaviour.strategy;
+
+import org.junit.Test;
+import org.learning.design.patterns.behaviour.strategy.runner.FileEditingTaskRunner;
+import org.learning.design.patterns.behaviour.strategy.strategies.AdvancedTaskExecutionStrategy;
+import org.learning.design.patterns.behaviour.strategy.strategies.BasicTaskExecutionStrategy;
+import org.learning.design.patterns.behaviour.strategy.strategies.IThreadStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * The idea for strategy pattern is that the client should not know about
+ * the algorithm or the strategy used.
+ * Decoupling the algorithm from the one who is using the algorithm
+ * for eg: List should provide sort functionality, but let client pass which sorting
+ * algorithm is required.
+ *
+ * In this test, we are providing the TaskExecution strategy to fileEditor
+ */
+public class ThreadStrategyDemoTest {
+
+    @Test
+    public void advancedTest(){
+        List<String> fileNames = new ArrayList<>();
+        fileNames.add("INPUT_MOCK_DATA_00.csv");
+        fileNames.add("INPUT_MOCK_DATA_01.csv");
+        fileNames.add("INPUT_MOCK_DATA_02.csv");
+        IThreadStrategy executionStrategy = new AdvancedTaskExecutionStrategy(new FileEditingTaskRunner(),fileNames);
+        FileEditor fileEditor = new FileEditor(executionStrategy);
+        fileEditor.performTask();
+    }
+
+    @Test
+    public void basicTaskExecutionTest(){
+        List<String> fileNames = new ArrayList<>();
+        fileNames.add("INPUT_MOCK_DATA_00.csv");
+        fileNames.add("INPUT_MOCK_DATA_01.csv");
+        fileNames.add("INPUT_MOCK_DATA_02.csv");
+        IThreadStrategy executionStrategy = new BasicTaskExecutionStrategy(new FileEditingTaskRunner(),fileNames);
+        FileEditor fileEditor = new FileEditor(executionStrategy);
+        fileEditor.performTask();
+    }
+}
