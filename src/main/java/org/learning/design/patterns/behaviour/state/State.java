2d26ffc9e59bcb3f29e3d12ace02d83be6a052e4
Vivek Agrawal <vivek.agrawal01@sap.com>
2020-04-18 18:12:58 +0530

    State Pattern

---
+++ src/main/java/org/learning/design/patterns/behaviour/state/State.java
@@ -0,0 +1,10 @@
+/*
+ * All rights reserved 2020
+ * author: vivek081141@gmail.com
+ *
+ */
+
+package org.learning.design.patterns.behaviour.state;
+
+public class State {
+}
---
+++ src/test/java/org/learning/design/patterns/behaviour/state/StateTest.java
@@ -0,0 +1,24 @@
+/*
+ * All rights reserved 2020
+ * author: vivek081141@gmail.com
+ *
+ */
+
+package org.learning.design.patterns.behaviour.state;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 1. Interface Based
+ * 2. Class per state
+ * 3. Each State knows about other state transition
+ * 4. Simplifies cyclomatic complexity - removes if-else
+ * 5. Adding new state is easier
+ *
+ * Drawbacks
+ * 1. More classes
+ * 
+ */
+public class StateTest {
+
+}
